8f806fe29ab25c68bacf3335445b1692df8d272a
brent couillard <brentcouillard@yahoo.com>
2023-02-04 09:43:43 -0600

    commit tree 4fb77d876ff4abf9b3e558f113c90e53ea6db3ae

--- src/index.js
+++ src/index.js
@@ -3,8 +3,28 @@ document.addEventListener("DOMContentLoaded", () => {
     getRamen()
 
 })
+const imageContainer = document.getElementById("ramenMenu")
 const submitRamen = document.getElementById("new-ramen")
 submitRamen.addEventListener("submit", createNewRamen)
+
+
+let currentRamen
+const deleteButton = document.getElementById("deleteButton")
+deleteButton.addEventListener("click", () => {
+    
+    fetch(`http://localhost:3000/ramens/${currentRamen.id}`, {
+        method: "DELETE"
+        
+    })
+    imageContainer.innerHTML = ""
+    getRamen()
+    
+})
+
+
+
+
+
 
 
 function getRamen() {
@@ -13,34 +33,50 @@ function getRamen() {
     .then (data => {
         const ramens = data
         ramens.map(ramen => displayImage(ramen))
+        ramenDetails(ramens[0])
     })
 }
 
 
 function displayImage(ramen) {
-    const imageContainer = document.getElementById("ramenMenu")
     const img = document.createElement("img")
     img.src = ramen.image
     img.addEventListener("click", () => ramenDetails(ramen))
     imageContainer.append(img)
 }
 
+ramenRating = document.getElementById("rating-display")
+const ramenComment = document.getElementById("comment-display")
 
 function ramenDetails(ramen) {
+    currentRamen = ramen
     const ramenImage = document.getElementById("detailImage")
     ramenImage.src = ramen.image
     const ramenName = document.getElementById("detailName")
     ramenName.innerText = ramen.name
     const restaurantName = document.getElementById("detailRestaurant")
     restaurantName.innerText = ramen.restaurant
-    const ramenRating = document.getElementById("rating-display")
     ramenRating.innerText = ramen.rating
-    const ramenComment = document.getElementById("comment-display")
     ramenComment.innerText = ramen.comment
+    
+   
+    
+}
+
+const editRamen = document.getElementById("edit-ramen")
+editRamen.addEventListener("submit", editDetails)
+
+function editDetails() {
+    event.preventDefault()
+    const newRating = document.getElementById("editedRating").value
+    ramenRating.innerText = newRating
+    const newComment = document.getElementById("editedComment").value
+    ramenComment.innerText = newComment
 
 }
 
-function createNewRamen() {
+
+function createNewRamen(event) {
     event.preventDefault()
     const name = document.getElementById("new-name").value
     const restaurant = document.getElementById("new-restaurant").value
@@ -48,5 +84,18 @@ function createNewRamen() {
     const rating = document.getElementById("new-rating").value
     const comment = document.getElementById("new-comment").value
     const ramen = {name, restaurant, image, rating, comment}
-    displayImage(ramen)
+    fetch("http://localhost:3000/ramens", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(ramen)
+    })
+    .then(res => res.json())
+    .then(data => {
+        event.target.reset()
+        displayImage(data)
+
+    })
 }
